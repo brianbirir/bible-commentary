3354314803053a1e8b2ec7dff6c4430fa0311d16
Brian Birir <brianbirir@pop-os.localdomain>
2023-04-21 14:36:56 +0300

    initial commit

---
+++ src/views/BibleView.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    <div class="about">
+      <h1>Sample Bible</h1>
+
+      <v-btn> Button </v-btn>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {}
+</script>
+<style lang=""></style>
